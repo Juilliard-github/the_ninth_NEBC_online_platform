5cba86341a32c715054263426349a0d8c2e242cc
Juilliard-github <juilliard.wynn@gmail.com>
2025-07-06 12:43:13 +0800

    update: 標題字體調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
 
 const HomePage = () => {
   const [isClient, setIsClient] = useState(false);
