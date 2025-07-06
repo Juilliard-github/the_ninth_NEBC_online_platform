21e277517ecc6cc2085e12ed8e4b1c39d98d7d2d
Juilliard-github <juilliard.wynn@gmail.com>
2025-07-06 11:53:20 +0800

    update: 標題更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,58 +16,21 @@ const HomePage = () => {
   return (
     <div className="font-[ChenYuluoyan] flex flex-col justify-center items-center">
       {/* 歡迎文字動畫 */}
-      <motion.svg
-        width="1200"
-        height="200"
-        viewBox="0 0 800 200"
-        xmlns="http://www.w3.org/2000/svg"
-        className="z-10 mx-auto">
-        {/* 使用motion.text進行逐字筆劃動畫 */}
-        <motion.text
-          x="50%" 
-          y="40%" 
-          dominantBaseline="middle"
-          textAnchor="middle"
-          fontSize="80" 
-          fontWeight="normal"
-          fill="black"
-          stroke="white"
-          strokeWidth='0.5'
-          initial={{ strokeDasharray: 800, strokeDashoffset: 800 }}  // 初始狀態，文字完全隱藏
-          animate={{ strokeDashoffset: 0 }}  // 動畫過程，逐筆顯示文字
-          transition={{ duration: 5, ease: 'easeInOut' }}  // 設定動畫時長與過渡效果
-        >
+      <svg width="1200" height="200" viewBox="0 0 800 200" xmlns="http://www.w3.org/2000/svg" className="z-10 mx-auto">
+        <text x="50%" y="40%" textAnchor="middle" dominantBaseline="middle" className="animated-stroke">
           歡迎來到
-        </motion.text>
-        <motion.text
-          x="50%" 
-          y="80%" 
-          dominantBaseline="middle"
-          textAnchor="middle"
-          fontSize="80" 
-          fontWeight="normal"
-          fill="black"
-          stroke="white"
-          strokeWidth='0.5'
-          initial={{ strokeDasharray: 800, strokeDashoffset: 800 }}  // 初始狀態，文字完全隱藏
-          animate={{ strokeDashoffset: 0 }}  // 動畫過程，逐筆顯示文字
-          transition={{ duration: 3, ease: 'easeInOut', delay: 0.5 }}  // 設定動畫時長與過渡效果
-        >
+        </text>
+        <text x="50%" y="80%" textAnchor="middle" dominantBaseline="middle" className="animated-stroke delay">
           竹科實中 9th 生研線上練習平台
-        </motion.text>
-      </motion.svg>
-      {/* 顯示更多文本 */}
-      <motion.p
-        className="text-4xl mt-6 drop-shadow-md"
-                      style={{ textShadow: '2px 2px 2px rgba(0, 0, 0, 0.15)' }}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3, delay: 0.5 }}
-      >
-        讓我們開始探索之旅，突破自我，迎接挑戰！
-      </motion.p>
+        </text>
+      </svg>
+
+      {/* 說明 */}
+      <p className="fade-in-text">讓我們開始探索之旅，突破自我，迎接挑戰！</p>
     </div>
   );
 }
 
 export default HomePage;
+
+
