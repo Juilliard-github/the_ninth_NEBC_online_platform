4b62aab00bae7b51ad328ce7bb4c1a420d7d7b9e
Juilliard-github <juilliard.wynn@gmail.com>
2025-07-07 09:16:47 +0800

    update: 更新排版

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,19 @@
 'use client'
-import React, { useEffect, useState } from 'react';
 
-const HomePage = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);  // Ensure that the code runs only in the client-side
-  }, []);
-
-  if (!isClient) {
-    return null;  // or some static fallback content while server-side rendering
-  }
-
+export default function HomePage() {
   return (
-    <div className="font-[ChenYuluoyan] flex flex-col justify-center items-center">
-      <div className='my-6 text-6xl font-bold text-center splash-text'>
-          歡迎回來<br/>竹科實中 9th 生研線上練習平台
+    <div className="relative z-10 p-8 max-w-5xl mx-auto space-y-8 font-[ChenYuluoyan] text-center max-h-screen overflow-y-hidden overflow-x-hidden">
+      {/* 歡迎標題 */}
+      <div className="my-6 text-4xl md:text-5xl lg:text-6xl font-medium splash-text">
+        ~ 歡迎回來 ~
+        <br />
+        竹科實中 9th 生研線上練習平台
       </div>
-      <p className="fade-in-text break-word">讓我們開始探索之旅，突破自我，迎接挑戰！</p>
+
+      {/* 介紹文字 */}
+      <div className="text-lg md:text-xl fade-in-text break-word font-light leading-relaxed">
+        讓我們開始探索之旅，突破自我，迎接挑戰！
+      </div>
     </div>
   )
 }
-
-export default HomePage;
